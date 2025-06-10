cdae556fe6602b7573dbbd97f3aeded491ac3df0
Andrey Bispo <andreybispof@gmail.com>
2025-06-10 12:24:23 -0300

    Correção no BelfryTwo_SecondRoom + BelfryFour_SecondRoom
    Alterações:
    - Add do script BelfryFour_SecondRoom
    
    Correção de bugs:
    - Correção no funcionamento da alavanca

--- Assets/Game/Code/Scripts/Puzzles/Belfry2/BelfryTwo_SecondRoom.cs
+++ Assets/Game/Code/Scripts/Puzzles/Belfry2/BelfryTwo_SecondRoom.cs
@@ -37,26 +37,39 @@ public class BelfryTwo_SecondRoom : MonoBehaviour
     }
     private void Update()
     {   
-        if (lever == playerInteraction.NextInteraction() && input.inputActions.Game.Interaction.WasPressedThisFrame())
+        delayLever -= Time.deltaTime; // Delay para poder usar a lever novamente.
+
+        // Se a alavanca for a interação mais próxima
+        if (lever == playerInteraction.NextInteraction())
         {
-            delayLever -= Time.deltaTime; // Delay para poder usar a lever novamente.
-
-            if (delayLever <= 0)
+            // Se o cooldown da alavanca tiver acabado
+            if (delayLever < 0)
             {
-                StartCoroutine(Platform(platformLever, platformLeverFinalPosition, durationPlatformLever));
-                delayLever = durationPlatformLever;
+                if (input.inputActions.Game.Interaction.WasPressedThisFrame())
+                {
+                    StartCoroutine(Platform(platformLever, platformLeverFinalPosition, durationPlatformLever)); // Levanta a plataforma.
+                    delayLever = durationPlatformLever + (time * 2); // Calcula o cooldown.
+                }
+                else
+                {
+                    playerInteraction.HasInteracted(false);
+                }
             }
-
+            else
+            {
+                playerInteraction.HasInteracted(true); // Não permite o prompt aparecer enquanto estiver no cooldown da alavanca.
+            }
         }
 
         if (target1.TryGetComponent<Target>(out var script))
         {
             delayTarget1 -= Time.deltaTime; // Delay para poder usar o alvo novamente.
 
+            // Se o alvo for acertado e o cooldown estiver terminado.
             if (script.WasCollided() && delayTarget1 <= 0)
             {
-                StartCoroutine(Platform(platformTarget1, platformTarget1FinalPosition, durationPlatformTarget1));
-                delayTarget1 = durationPlatformTarget1;
+                StartCoroutine(Platform(platformTarget1, platformTarget1FinalPosition, durationPlatformTarget1)); // Levanta a plataforma.
+                delayTarget1 = durationPlatformTarget1 + (time * 2); // Calcula o cooldown.
             }
         }
 
@@ -64,10 +77,11 @@ public class BelfryTwo_SecondRoom : MonoBehaviour
         {
             delayTarget2 -= Time.deltaTime; // Delay para poder usar o alvo novamente.
 
+            // Se o alvo for acertado e o cooldown estiver terminado.
             if (script2.WasCollided() && delayTarget2 <= 0)
             {
-                StartCoroutine(Platform(platformTarget2, platformTarget2FinalPosition, durationPlatformTarget2));
-                delayTarget2 = durationPlatformTarget2;
+                StartCoroutine(Platform(platformTarget2, platformTarget2FinalPosition, durationPlatformTarget2)); // Levanta a plataforma.
+                delayTarget2 = durationPlatformTarget2 + (time * 2); // Calcula o cooldown.
             }
         }
     }
---
+++ Assets/Game/Code/Scripts/Puzzles/Belfry4/BelfryFour_SecondRoom.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+
+public class BelfryFour_SecondRoom : MonoBehaviour
+{
+    /*[SerializeField, Tooltip("Os objetos dem estar na layer InteractableObjects.")] GameObject computers; // O computador na cena.
+    [SerializeField, Tooltip("Precisam ter suas MeshRenderer ativadas para serem desativadas quando o player interagir com cada computador.")] GameObject screenComputers; // A tela do computador na cena.
+    [SerializeField] GameObject[] visualOrder = new GameObject[4];
+    [SerializeField] GameObject door;
+
+    private InputActionsManager input;
+    private PlayerInteraction playerInteraction;
+
+    private int[] order = { 1, 2, 3, 4 }; // Cada n° representa um elemento do array computers. Se a ordem for 3 , 2, ..., então o computador 3 será o primeiro e assim por diante.
+    private int attempt = 0;
+    private bool orderIsWrong;
+    private void Start()
+    {
+        input = InputActionsManager.Instance;
+        playerInteraction = FindFirstObjectByType<PlayerInteraction>();
+
+        ChangeOrder();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < computers.Length; i++)
+        {
+            if (computers[i] != null && screenComputers[i])
+            {
+                if (computers[i] == playerInteraction.NextInteraction())
+                {
+                    if (input.inputActions.Game.Interaction.WasPressedThisFrame())
+                    {
+                        if (!screenComputers[i].GetComponent<MeshRenderer>().enabled)
+                        {
+                            screenComputers[i].GetComponent<MeshRenderer>().enabled = true;
+                            TryOrder();
+                        }
+                    }
+                }
+                else
+                {
+                    if (computers[i].TryGetComponent<Outline>(out var outline))
+                    {
+                        outline.enabled = false;
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void TryOrder()
+    {
+        if (screenComputers[order[attempt] - 1].GetComponent<MeshRenderer>().enabled == false)
+        {
+            orderIsWrong = true;
+        }
+
+        attempt++;
+
+        if (attempt >= 4)
+        {
+            if (orderIsWrong)
+            {
+                ChangeOrder();
+                orderIsWrong = false;
+                attempt = 0;
+            }
+            else
+            {
+                if (door != null)
+                {
+                    door.SetActive(false); // Abrir a porta.
+                }
+
+                // Finaliza o puzzle.
+                enabled = false;
+            }
+        }
+    }
+
+    private void ChangeOrder()
+    {
+        for (int i = 0; i < order.Length; i++)
+        {
+            int j = Random.Range(0, order.Length);
+
+            // Embaralha os arrays.
+            (order[j], order[i]) = (order[i], order[j]);
+        }
+
+        for (int i = 0; i < visualOrder.Length; i++)
+        {
+            // Coloca a ordem correta visível para o player.
+            visualOrder[i].transform.position = screenComputers[order[i] - 1].transform.position;
+        }
+
+        foreach (var screen in screenComputers)
+        {
+            // Desabilita a tela de cada computador.
+            screen.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        // Faz com que o player ainda não tenha interagido com nenhum objeto(Isso é mais para o visual, código no PlayerInteraction).
+        playerInteraction.HasInteracted(false);
+    }*/
+}
